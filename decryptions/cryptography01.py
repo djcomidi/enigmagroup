6c66aa9fca6444b55531be97fd02bfdfbb83e6b8
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-01-07 22:50:54 +0100

    decryptions: 01: adding solution

---
+++ decryptions/cryptography01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def rot13(c):
+	start = ord("A")
+	if c in "abcdefghijklmnopqrstuvwxyz": start = ord("a")
+	return chr(start + ((ord(c) - start + 13) % 26))
+
+s="EbGguvEgrRavfNcvRprbsShpXvatCvFf"
+
+sol=""
+for c in s:
+	sol += rot13(c)
+print sol
+
