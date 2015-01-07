f98dd6c7b6bbec574a542b7e4435c26c12891a53
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-01-07 13:59:54 +0100

    basic: js: 07: adding solution

---
+++ basic/js/07/solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+s = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+s = s[::-1]
+sol = []
+sol.append( s[42] )
+sol.append( s[11] )
+sol.append( s[17] )
+sol.append( s[12] )
+sol.append( s[7] )
+sol.append( s[43] )
+sol.append( s[6] )
+print "_".join(sol)
