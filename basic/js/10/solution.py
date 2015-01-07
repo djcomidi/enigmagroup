6f91be857feac41056177f489fdd663cc997a0d0
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-01-07 18:56:10 +0100

    basic: js: 10: adding solution

---
+++ basic/js/10/solution.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+
+def calculate(s,n):
+	ret = 0
+	for c in s:
+		ret += ord(c) * n
+		n += 1
+	return ret
+
+def search_passwords(passw=""):
+	pc = calculate(passw,3)
+	if pc == 2308:
+		print passw
+		sys.exit() # only one password is enough
+	elif pc > 2308:
+		return
+	else:
+		for c in "abcdefghijklmnopqrstuvwxyz":
+			search_passwords(passw+c)
+			search_passwords(passw+c.upper())
+
+REF = "BHFE8"
+REFSUM = calculate(REF,5)
+print REFSUM
+search_passwords()
