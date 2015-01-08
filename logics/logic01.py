39fe72ea38baf3e1710e95556e042fa009e893fe
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-01-08 10:41:03 +0100

    logics: 01: adding solution

---
+++ logics/logic01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from itertools import permutations
+
+nBreads=3
+
+nMeats=4
+
+cheeses=set()
+for p in permutations('000ASGC',r=2):
+	cheeses.add( ''.join(sorted(p)) )
+nCheeses=len(cheeses)
+
+toppings=set()
+for p in permutations('000ABCDEFGH',r=3):
+	toppings.add( ''.join(sorted(p)) )
+nToppings=len(toppings)
+
+print nBreads * nMeats * nCheeses * nToppings
