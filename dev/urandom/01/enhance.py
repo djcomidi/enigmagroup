4398b59efa53fc29f04e838a88c042aaf8fd1ce6
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-01-10 16:42:26 +0100

    dev: urandom: 01: adding solution

---
+++ dev/urandom/01/enhance.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from PIL import Image
+
+imgA = Image.open('note.png')
+print list(imgA.getcolors())
+data = list(imgA.getdata())
+newdata = []
+for d in data:
+	if d == (21,21,22,255): newdata.append((255,255,255))
+	else: newdata.append((0,0,0))
+imgB = Image.new("RGB",imgA.size)
+imgB.putdata(newdata)
+imgB.save('noteB.png')
+
